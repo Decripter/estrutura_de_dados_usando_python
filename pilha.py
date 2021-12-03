459150afbaa93e77d78d5bfd9c6056642f7f7c32
Decripter <jeilsonaraujo@icloud.com>
2021-12-02 22:30:27 -0300

    feat: adicionado a funcao desempilhar

--- pilha.py
+++ pilha.py
@@ -1,10 +1,16 @@
 class Pilha:
-    vazia = False
+    vazia = True
     pilha_conteudo = []
 
     def __init__(self, ):
         pass
 
     def empilhar(self, item):
+        if self.vazia == True:
+            self.vazia = False
         self.pilha_conteudo.append(item)
         return item
+
+    def desempilhar(self):
+        desempilhado = self.pilha_conteudo.pop()
+        return desempilhado
