a31f550a347864b3984e9dbc68dad09a0feca9ef
Decripter <jeilsonaraujo@icloud.com>
2021-12-05 10:08:15 -0300

    feat: adicionando outra estrutura de dados a fila

---
+++ fila.py
@@ -0,0 +1,20 @@
+class fila:
+
+    def __init__(self):
+        fila_conteudo = []
+
+    def enfileirar(self):
+        ...
+
+    def desenfileirar(self):
+        ...
+
+    def mostrar_inicio_da_fila(self):
+        ...
+
+    def mostrar_tamanho_da_fila(self):
+        ...
+
+    def limpar_fila(self):
+        ...
+        
