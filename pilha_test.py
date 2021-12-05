76c777c72976a5afe5d8118b6ff2c95ae5656b0a
Decripter <jeilsonaraujo@icloud.com>
2021-12-05 06:59:38 -0300

    test: adicionando uma fixture e corrigindo um teste que estava com o assert errado

--- pilha_test.py
+++ pilha_test.py
@@ -1,27 +1,27 @@
+from pytest import fixture
 import pytest
 import pilha
 
-
+@fixture
+def nova_pilha():
+    pilha_test = pilha.Pilha()
+    yield pilha_test
+    
 
 # metodo empilhar
-def test_empilhar():
-    nova_pilha = pilha.Pilha()
+def test_empilhar(nova_pilha):
     test = nova_pilha.empilhar(3)
-    del(nova_pilha)
     assert test == 3
 
 
 # metodo desenpilhar
-def test_desempilhar():
-    nova_pilha = pilha.Pilha()
+def test_desempilhar(nova_pilha):
     nova_pilha.empilhar(3)
     nova_pilha.empilhar(5)
     nova_pilha.empilhar(10)
     test = nova_pilha.desempilhar()
-    nova_pilha.desempilhar()
-    nova_pilha.desempilhar()
-    nova_pilha.desempilhar()
-    del(nova_pilha)
+    nova_pilha.esvaziar_pilha()
+    nova_pilha.imprimir_pilha()
     assert test == 10
 
 
@@ -33,36 +33,29 @@ def test_desempilhar():
 
 
 # imprimir a pilha completa
-def test_imprimir_pilha():
-    nova_pilha = pilha.Pilha()
+def test_imprimir_pilha(nova_pilha):
     nova_pilha.empilhar(1)
     nova_pilha.empilhar(2)
     nova_pilha.empilhar(3)
     nova_pilha.empilhar(4)
     test = nova_pilha.imprimir_pilha()
     esperado = [1,2,3,4]
-    del(nova_pilha)
     assert test == esperado
 
 
 # checar se a pilha esta vazia
 
-def test_pilha_vazia():
-    nova_pilha = pilha.Pilha()
+def test_pilha_vazia(nova_pilha):
     assert nova_pilha.vazia == True
 
-def test_pilha_nao_vazia():
-    nova_pilha = pilha.Pilha()
+def test_pilha_nao_vazia(nova_pilha):
     nova_pilha.empilhar(2)
     test = nova_pilha.vazia
-    del(nova_pilha)
     assert test == False
 
-def test_pilha_desempilhar_deixando_vazia():
-    nova_pilha = pilha.Pilha()
+def test_pilha_desempilhar_deixando_vazia(nova_pilha):
     nova_pilha.empilhar(2)
     nova_pilha.desempilhar()
     test = nova_pilha.vazia
-    del(nova_pilha)
-    assert test == False
+    assert test == True
     
